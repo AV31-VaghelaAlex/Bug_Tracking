b1647fac6327a9ec8050a36cd66a73f25d6f94a8
AV31-VaghelaAlex <alexvaghela31@gmail.com>
2026-09-23 11:52:38 +0000

    UPDATE: bugs/views.py

--- bugs/views.py
+++ bugs/views.py
@@ -1,10 +1,8 @@
-
-from django.shortcuts import render,redirect,get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Bug
-from .forms import BugForm,CommentForm
+from .forms import BugForm, CommentForm
 
-@login_required
+
 def dashboard(request):
     bugs = Bug.objects.all()
 
@@ -17,29 +15,57 @@ def dashboard(request):
     if status:
         bugs = bugs.filter(status=status)
 
-    return render(request,'bugs/dashboard.html',{'bugs':bugs})
+    return render(
+        request,
+        "bugs/dashboard.html",
+        {"bugs": bugs}
+    )
 
-@login_required
+
 def create_bug(request):
-    form=BugForm(request.POST or None, request.FILES or None)
+    form = BugForm(request.POST or None, request.FILES or None)
+
     if form.is_valid():
         form.save()
-        return redirect('/')
-    return render(request,'bugs/form.html',{'form':form})
+        return redirect("/")
 
-@login_required
-def bug_detail(request,pk):
-    bug=get_object_or_404(Bug,pk=pk)
-    form=CommentForm(request.POST or None)
+    return render(
+        request,
+        "bugs/form.html",
+        {"form": form}
+    )
+
+
+def bug_detail(request, pk):
+    bug = get_object_or_404(Bug, pk=pk)
+
+    form = CommentForm(request.POST or None)
+
     if form.is_valid():
-        c=form.save(commit=False)
-        c.bug=bug
-        c.user=request.user
-        c.save()
-        return redirect(f'/bug/{pk}/')
-    return render(request,'bugs/detail.html',{'bug':bug,'form':form})
+        c = form.save(commit=False)
+        c.bug = bug
 
-@login_required
+        # Since we are not using login anymore,
+        # don't use request.user here.
+        c.save()
+
+        return redirect(f"/bug/{pk}/")
+
+    return render(
+        request,
+        "bugs/detail.html",
+        {
+            "bug": bug,
+            "form": form
+        }
+    )
+
+
 def my_bugs(request):
-    bugs = Bug.objects.filter(assigned_to=request.user)
-    return render(request,"bugs/my_bugs.html",{"bugs":bugs})
+    bugs = Bug.objects.all()
+
+    return render(
+        request,
+        "bugs/my_bugs.html",
+        {"bugs": bugs}
+    )
